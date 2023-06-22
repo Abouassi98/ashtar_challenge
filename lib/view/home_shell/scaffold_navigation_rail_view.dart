5332462638c7473b3e86d5397e587adcd9cbff1c
Abouassi98 <mohamedabouassi96@gmail.com>
2023-06-22 15:01:13 +0300

    make a nav rail as a scrollable

--- lib/view/home_shell/scaffold_navigation_rail_view.dart
+++ lib/view/home_shell/scaffold_navigation_rail_view.dart
@@ -22,66 +22,79 @@ class ScaffoldWithNavigationRail extends StatelessWidget {
           // Fixed navigation rail on the left (start)
           Flexible(
             flex: 1,
-            child: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Color(
-                      0xff3E7082,
-                    ),
-                    Color(
-                      0xff2F6A7C,
-                    ),
-                    Color(
-                      0xff44767B,
-                    ),
-                    Color(
-                      0xff44767B,
-                    ),
-                  ],
+            child: SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: MediaQuery.sizeOf(context).height,
                 ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(top: Sizes.appBarHeight56),
-                child: NavigationRail(
-                  selectedIndex: selectedIndex,
-                  selectedIconTheme: const IconThemeData(color: Colors.red),
-                  backgroundColor: Colors.transparent,
-                  onDestinationSelected: onDestinationSelected,
-                  labelType: NavigationRailLabelType.selected,
-                  destinations: const [
-                    NavigationRailDestination(
-                      label: SizedBox(),
-                      selectedIcon: CustomSelectedIcon(
-                          icon: Icons.home, title: 'DashBoard'),
-                      icon: CustomIcon(icon: Icons.home, title: 'DashBoard'),
+                child: IntrinsicHeight(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Color(
+                            0xff3E7082,
+                          ),
+                          Color(
+                            0xff2F6A7C,
+                          ),
+                          Color(
+                            0xff44767B,
+                          ),
+                          Color(
+                            0xff44767B,
+                          ),
+                        ],
+                      ),
                     ),
-                    NavigationRailDestination(
-                      label: SizedBox(),
-                      icon: CustomIcon(icon: Icons.schedule, title: 'Schedule'),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: Sizes.appBarHeight56),
+                      child: NavigationRail(
+                        selectedIndex: selectedIndex,
+                        selectedIconTheme:
+                            const IconThemeData(color: Colors.red),
+                        backgroundColor: Colors.transparent,
+                        onDestinationSelected: onDestinationSelected,
+                        labelType: NavigationRailLabelType.selected,
+                        destinations: const [
+                          NavigationRailDestination(
+                            label: SizedBox(),
+                            selectedIcon: CustomSelectedIcon(
+                                icon: Icons.home, title: 'DashBoard'),
+                            icon: CustomIcon(
+                                icon: Icons.home, title: 'DashBoard'),
+                          ),
+                          NavigationRailDestination(
+                            label: SizedBox(),
+                            icon: CustomIcon(
+                                icon: Icons.schedule, title: 'Schedule'),
+                          ),
+                          NavigationRailDestination(
+                            label: SizedBox(),
+                            icon: CustomIcon(
+                                icon: Icons.computer_sharp, title: 'Courses'),
+                          ),
+                          NavigationRailDestination(
+                            label: SizedBox(),
+                            icon: CustomIcon(
+                                icon: Icons.grade, title: 'Gradebook'),
+                          ),
+                          NavigationRailDestination(
+                            label: SizedBox(),
+                            icon: CustomIcon(
+                                icon: Icons.settings, title: 'performance'),
+                          ),
+                          NavigationRailDestination(
+                            label: SizedBox(),
+                            icon: CustomIcon(
+                                icon: Icons.announcement, title: 'Announcment'),
+                          ),
+                        ],
+                      ),
                     ),
-                    NavigationRailDestination(
-                      label: SizedBox(),
-                      icon: CustomIcon(
-                          icon: Icons.computer_sharp, title: 'Courses'),
-                    ),
-                    NavigationRailDestination(
-                      label: SizedBox(),
-                      icon: CustomIcon(icon: Icons.grade, title: 'Gradebook'),
-                    ),
-                    NavigationRailDestination(
-                      label: SizedBox(),
-                      icon: CustomIcon(
-                          icon: Icons.settings, title: 'performance'),
-                    ),
-                    NavigationRailDestination(
-                      label: SizedBox(),
-                      icon: CustomIcon(
-                          icon: Icons.announcement, title: 'Announcment'),
-                    ),
-                  ],
+                  ),
                 ),
               ),
             ),
